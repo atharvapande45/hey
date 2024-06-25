5a48e291ef379deb601bda80eec6a388fc0c20ec
atharvapande45 <atharva.pande20@pccoepune.org>
2024-06-25 17:34:09 +0530

    sock

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 
 export default function App() {
     const [statement, setStatement] = useState("I am sorry");
-    const [index, setIndex] = useState(1);
+    const [index, setIndex] = useState(5499999);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setStatement(`I am sorry ${index}`);
-            setIndex((prev) => prev + 1);
+            if (index < 5500000) {
+                setStatement(`I am sorry ${index}`);
+                setIndex((prev) => prev + 1);
+            }
         }, 0.0001);
         return () => clearInterval(intervalId);
     }, [index]);
@@ -16,6 +18,7 @@ export default function App() {
         <div>
             <h1>{statement}</h1>
             <p>55 lakh sorries incoming 🦸‍♂️</p>
+            <p>I am becoming your sock next life 🧦</p>
         </div>
     );
 }
