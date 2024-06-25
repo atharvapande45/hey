c325ee5e89cd99a849e938dddbbd9fea962a9f17
atharvapande45 <atharva.pande20@pccoepune.org>
2024-06-25 17:37:12 +0530

    final optis

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import React, { useState, useEffect } from "react";
 
 export default function App() {
     const [statement, setStatement] = useState("I am sorry");
-    const [index, setIndex] = useState(1);
+    const [index, setIndex] = useState(0);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            if (index < 5500000) {
+            if (index <= 5500000) {
                 setStatement(`I am sorry ${index}`);
                 setIndex((prev) => prev + 100000);
             }
-        }, 0.5);
+        }, 10);
         return () => clearInterval(intervalId);
-    }, [index]);
+    });
 
     return (
         <div>
