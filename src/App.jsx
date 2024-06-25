e9b4ca0a41d09de0cbe9728bccf196a64d0be9b6
atharvapande45 <atharva.pande20@pccoepune.org>
2024-06-25 17:16:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+export default function App() {
+    const [statement, setStatement] = useState("I am sorry");
+    const [index, setIndex] = useState(1);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setStatement(`I am sorry ${index}`);
+            setIndex((prev) => prev + 1);
+        }, 0.0001);
+        return () => clearInterval(intervalId);
+    }, [index]);
+
+    return (
+        <div>
+            <h1>{statement}</h1>
+        </div>
+    );
+}
